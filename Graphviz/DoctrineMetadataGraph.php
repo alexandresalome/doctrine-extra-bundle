a517922200a8523b097b8688e1773ba9ebf88eb3
Robert (Jamie) Munro <rjmunro@arjam.net>
2014-07-10 09:40:30 +0100

    Make clustering work for \ as well as : separators

--- Graphviz/DoctrineMetadataGraph.php
+++ Graphviz/DoctrineMetadataGraph.php
@@ -99,12 +99,9 @@ class DoctrineMetadataGraph extends Digraph
 
     private function getCluster($entityName)
     {
-        $exp = explode(':', $entityName);
+        $exp = preg_split('/\\\\|:/', $entityName);
+        $name = array_pop($exp);
 
-        if (count($exp) !== 2) {
-            throw new \OutOfBoundsException(sprintf('Unexpected count of ":" in entity name. Expected one ("AcmeDemoBundle:User"), got %s ("%s").', count($exp), $entityName));
-        }
-
-        return $exp[0];
+        return implode('_', $exp);
     }
 }
